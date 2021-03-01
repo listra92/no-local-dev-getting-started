4c35e5a4b9a4e044ee0f6e3b18bd2882f28b0383
listra92 <listra92@gmail.com>
2021-03-01 09:02:55 +0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -25,14 +25,6 @@ get "/ran" do
   erb :ran
 end
 
-get "/create" do
-  dashboard_url = 'https://dashboard.heroku.com/'
-  match = /(.*?)\.herokuapp\.com/.match(request.host)
-  dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
-  redirect to(dashboard_url)
-end
-
-
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
@@ -40,4 +32,11 @@ end
 get "/contacts" do
   @contacts = Contact.all
   erb :index
+end
+
+get "/create" do
+  dashboard_url = 'https://dashboard.heroku.com/'
+  match = /(.*?)\.herokuapp\.com/.match(request.host)
+  dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
+  redirect to(dashboard_url)
 end
